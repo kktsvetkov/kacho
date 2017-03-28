54f3db51b511468954c52823821c72a063f9a7db
Kaloyan K. Tsvetkov <kt@Ultron.local>
2017-03-28 10:31:30 +0300

    minor reformatting

--- kacho.php
+++ kacho.php
@@ -2,23 +2,21 @@
 /**
 * Kacho: File-based PHP5 caching library
 *
-* @license http://www.gnu.org/licenses/lgpl.html
+* @license http://opensource.org/licenses/LGPL-3.0 GNU Lesser General Public License, version 3.0
 * @author Kaloyan K. Tsvetkov <kalotan@kaloyan.info>
-* @link http://code.google.com/p/kacho/ 
+* @link https://github.com/kktsvetkov/kacho/
 */
-
-///////////////////////////////////////////////////////////////////////////////
 
 /**
 * Kacho: File-based PHP5 caching library
 */
-class Kacho {
-
+class Kacho
+{
 	/**
 	* @var string filename where the cache is stored
 	*/
 	protected $filename = null;
-	
+
 	/**
 	* @var string
 	*/
@@ -27,27 +25,26 @@ class Kacho {
 	/**
 	* @var string
 	*/
-	protected $chmod = 02775;	
-
-	// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+	protected $chmod = 02775;
 
 	/**
 	* Constructor
 	*
 	* @param string $filename
 	* @param array $options
-	*/	
-	public function __construct($filename, array $options = array()) {
+	*/
+	public function __construct($filename, array $options = array())
+	{
 		$this->filename = $filename;
-		
+
 		$options += array(
 			'chgrp' => $this->chgrp,
 			'chmod' => $this->chmod
 			);
 		$this->chgrp = $options['chgrp'];
 		$this->chmod = $options['chmod'];
-		}
-	
+	}
+
 	/**
 	* Static shortcut method (for piping it with {@link Kacho::read()}
 	* and {@link Kacho::write()}, e.g. Kachoo:open('x')->write(23);)
@@ -56,54 +53,57 @@ class Kacho {
 	* @param array $options
 	* @return Kacho
 	*/
-	public static function open($filename, array $options = array()) {
+	public static function open($filename, array $options = array())
+	{
 		return new Kacho($filename, $options);
-		}
-	
+	}
+
 	/**
 	* Reads the stored data; if there is nothing then it returns FALSE
 	* @return mixed|FALSE
 	*/
-	public function read() {
+	public function read()
+	{
 		@include($this->filename);
 		$c = 'kacho_' . md5($this->filename);
 		return isset($$c) ? $$c : false;
-		}
+	}
 
 	/**
 	* Writes data in the file.
 	*
-	* @param mixed $data 
+	* @param mixed $data
 	* @param integer $expire the time-to-live (TTL) of the cached data
 	* @return boolean
 	*/
-	public function write($data, $expire = 3600) {
-		
+	public function write($data, $expire = 3600)
+	{
+
 		$tmp_name = tempnam(dirname($this->filename), basename($this->filename));
 		$md5 = md5($this->filename);
 
 		@file_put_contents(
 			$tmp_name,
-			'<?php if(' . intval(time() + $expire) . '>time())$kacho_' 
+			'<?php if(' . intval(time() + $expire) . '>time())$kacho_'
 				. $md5 . '=' . var_export($data, 1)
 				. ';',
 			LOCK_EX
 			);
 		@unlink($this->filename);
 		@rename($tmp_name, $this->filename);
-		
-		if ($this->chgrp) {
+
+		if ($this->chgrp)
+		{
 			@chgrp($this->filename, $this->chgrp);
-			}
-			
-		if ($this->chmod) {
+		}
+
+		if ($this->chmod)
+		{
 			@chmod($this->filename, $this->chmod);
-			}
-		
+		}
+
 		// compare the original data and the stored one
 		//
 		return $data == $this->read();
-		}
-	
-	////--end-of-class----
 	}
+}
